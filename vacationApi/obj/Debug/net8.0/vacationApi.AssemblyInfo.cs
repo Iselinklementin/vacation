6bb6cc6495a83328c9215509b55e180dcee4cb77
Iselinklementin <69785627+Iselinklementin@users.noreply.github.com>
2024-04-28 16:42:20 +0200

    init

--- vacationApi/obj/Debug/net8.0/vacationApi.AssemblyInfo.cs
+++ vacationApi/obj/Debug/net8.0/vacationApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("vacationApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6298b033797705615cc1a620aac8d846664d8092")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9a73d17065c5b644f172cac77cda201e5b3ab6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("vacationApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("vacationApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
