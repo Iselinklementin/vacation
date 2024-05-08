b9169929eb58b99ee3853fd524bada7c76330d90
Iselin Haugen <69785627+Iselinklementin@users.noreply.github.com>
2024-05-08 09:59:43 +0200

    Update vue.config.js

--- avonova/vue.config.js
+++ avonova/vue.config.js
@@ -1,13 +1,10 @@
 const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
-  /*  indexPath: "public/index.html", */
   transpileDependencies: true,
-  /*  publicPath: "", */
   chainWebpack: (config) => {
     config.plugin("html").tap((args) => {
       args[0].title = "Ferieregistrering";
-      /*    args[0].filename = "[name].html"; */
       return args;
     });
   },
